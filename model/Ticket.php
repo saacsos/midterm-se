85329261daacb8d75ddeb058aeab003d43c41019
Saac Sornchai <saacsos@gmail.com>
2019-03-16 14:46:26 +0700

    Ticket Model properties

--- model/Ticket.php
+++ model/Ticket.php
@@ -1,4 +1,33 @@
 <?php
 class Ticket {
-    
+    private $sourceStation;
+    private $destinationStation;
+    private $trainNumber;
+    private $departureTime;
+    private $arrivalTime;
+    private $bogieNumber;
+    private $seatNumber;
+
+    public function __construct($trainNumber)
+    {
+        $this->trainNumber = $trainNumber;
+    }
+
+    public function setTime($departure, $arrival)
+    {
+        $this->departureTime = $departure;
+        $this->arrivalTime = $arrival;
+    }
+
+    public function setSeat($bogieNumber, $seatNumber)
+    {
+        $this->bogieNumber = $bogieNumber;
+        $this->seatNumber = $seatNumber;
+    }
+
+    public function setStation($source, $destination)
+    {
+        $this->sourceStation = $source;
+        $this->destinationStation = $destination;
+    }
 }
